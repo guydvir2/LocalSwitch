b6599c25517106cbc926fc5c44eb4b9a90f5fd83
guyd@linux <guydvir.tech@gmail.com>
2018-07-21 22:24:18 +0300

    try to change DS conecpt

--- ParentsRoomWindow/DSswitch.py
+++ ParentsRoomWindow/DSswitch.py
@@ -23,32 +23,46 @@ def mqtt_commands(msg):
             device_name + sw1_name, loc_double_switch.switch.switch1.switch_state[0],
             loc_double_switch.switch.switch1.switch_state[1])
 
-        mqtt_agent.pub(topic=msg_topic, payload=state_0+'\n'+state_1)
-        #mqtt_agent.pub(topic= msg_topic, payload=state_1)
+        mqtt_agent.pub(topic=msg_topic, payload=state_0 + '\n' + state_1)
     else:
         print(msg, 'Unrecognized command')
 
 
+def read_conf_file(confile):
+    global file_param
+    if os.path.isfile(confile) is True:
+        with open(confile, 'r') as f:
+            data_file = f.readlines()
+            for line in data_file:
+                file_param[line.split('=')[0].strip()] = line.split('=')[1].split('\n')[0].strip()
+    else:
+        print('file', confile, ' not found')
+        quit()
+
+
 ################## Path Parameters ##################
+file_param={}
+
 base_path = '/home/guy/github'
 main_path = base_path + '/LocalSwitch'
-sub_path = '/ParentsRoomWindow'
+sub_path = file_param["SUB_PATH"]
 mod_path = base_path + '/RemoteSwitch'
 homedir = main_path + sub_path
 path.append(mod_path)
 path.append(main_path)
 
-from localswitches import HomePiLocalSwitch
+# from localswitches import HomePiLocalSwitch
 from mqtt_switch import MQTTClient
-import getip
+
+# import getip
 
 ######################################################
 
 ################## Switch Parameters #################
-device_name = 'P-RoomWindow'
+device_name = file_param["DEVICE_NAME"]
 switch_type = 'double'
-gpio_in = [20, 21]
-gpio_out = [19, 26]
+gpio_in = file_param["GPIO_IN"]
+gpio_out = file_param["GPIO_OUT"]
 mode = 'press'
 ext_log = homedir + '/%s.log' % device_name
 recps = ['guydvir.tech@gmail.com']
@@ -61,7 +75,7 @@ sw1_name = '/Down'
 ########################  Schedule 0  #################
 # Select One
 local_schedule_0 = None
-sched_filename_0 = homedir + '/sched_up.txt'
+sched_filename_0 = homedir + file_param["SCHED_UP"]
 #######################################################
 
 
@@ -69,7 +83,7 @@ sched_filename_0 = homedir + '/sched_up.txt'
 # Select One
 # DoubleSwitch only
 local_schedule_1 = None
-sched_filename_1 = homedir + '/sched_down.txt'
+sched_filename_1 = homedir + file_param["SCHED_DOWN"]
 #######################################################
 
 
@@ -88,39 +102,43 @@ msg_topic = main_topic + 'Messages'
 device_topic = group_topic + device_name
 
 #######################################################
+
 # Run Switch
-loc_double_switch = HomePiLocalSwitch(switch_type=switch_type,
-                                      gpio_in=gpio_in, gpio_out=gpio_out, mode=mode,
-                                      ext_log=ext_log, alias=device_name, sw0_name=sw0_name,
-                                      sw1_name=sw1_name)
-
-# Run Watch_dog service
-loc_double_switch.use_watch_dog()
-
-# Run Local schedule
-loc_double_switch.weekly_schedule(local_schedule_0=local_schedule_0,
-                                  sched_filename_0=sched_filename_0, local_schedule_1=local_schedule_1,
-                                  sched_filename_1=sched_filename_1)
-
-# Run Gmail defs
-loc_double_switch.gmail_defs(recipients=recps, sender_file=s_file,
-                             password_file=p_file)
-
-# Notify after boot
-loc_double_switch.notify_by_mail(subj='HomePi:%s boot summery' % device_name,
-                                 body='Device loaded successfully @%s' % getip.get_ip()[0])
-
-# Run MQTT protocol
-mqtt_agent = MQTTClient(sid=device_name, topic=device_topic, topic_qos=0, host=mqtt_host)
-mqtt_agent.call_externalf = lambda: mqtt_commands(mqtt_agent.arrived_msg)
-mqtt_agent.start()
-
-# Boot test
-if switch_type == 'double':
-    loc_double_switch.switch.switch0.switch_state = 1
-    sleep(0.5)
-    loc_double_switch.switch.switch0.switch_state = 0
-    sleep(0.5)
-    loc_double_switch.switch.switch1.switch_state = 1
-    sleep(0.5)
-    loc_double_switch.switch.switch1.switch_state = 0
+read_conf_file(homedir + '/DSswitch.conf')
+# loc_double_switch = HomePiLocalSwitch(switch_type=switch_type,
+#                                       gpio_in=gpio_in, gpio_out=gpio_out, mode=mode,
+#                                       ext_log=ext_log, alias¬=device_name, sw0_name=sw0_name,
+#                                       sw1_name=sw1_name)
+#
+# # Run Watch_dog service
+# loc_double_switch.use_watch_dog()
+#
+# # Run Local schedule
+if file_param["ENABLE_SCHED"]=='True':
+    print("YES")
+# loc_double_switch.weekly_schedule(local_schedule_0=local_schedule_0,
+#                                   sched_filename_0=sched_filename_0, local_schedule_1=local_schedule_1,
+#                                   sched_filename_1=sched_filename_1)
+#
+# # Run Gmail defs
+# loc_double_switch.gmail_defs(recipients=recps, sender_file=s_file,
+#                              password_file=p_file)
+#
+# # Notify after boot
+# loc_double_switch.notify_by_mail(subj='HomePi:%s boot summery' % device_name,
+#                                  body='Device loaded successfully @%s' % getip.get_ip()[0])
+#
+# # Run MQTT protocol
+# mqtt_agent = MQTTClient(sid=device_name, topic=device_topic, topic_qos=0, host=mqtt_host)
+# mqtt_agent.call_externalf = lambda: mqtt_commands(mqtt_agent.arrived_msg)
+# mqtt_agent.start()
+#
+# # Boot test
+# if switch_type == 'double':
+#     loc_double_switch.switch.switch0.switch_state = 1
+#     sleep(0.5)
+#     loc_double_switch.switch.switch0.switch_state = 0
+#     sleep(0.5)
+#     loc_double_switch.switch.switch1.switch_state = 1
+#     sleep(0.5)
+#     loc_double_switch.switch.switch1.switch_state = 0
