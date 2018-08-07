3aebc9e3656c1b3bed8db990882414c8bbd5c2d6
guyd@linux <guydvir.tech@gmail.com>
2018-08-07 22:17:21 +0300

    class is almort ready

--- main/Sched_MQTT.py
+++ main/Sched_MQTT.py
@@ -4,8 +4,8 @@ from time import sleep
 import os
 import sys
 
-mod_path='/home/guy/github/modules/'
-main_path='/home/guy/github/RemoteSwitch'
+mod_path = '/home/guy/github/modules/'
+main_path = '/home/guy/github/RemoteSwitch'
 path.append(mod_path)
 path.append(main_path)
 
@@ -13,119 +13,85 @@ import scheduler
 from mqtt_switch import MQTTClient
 
 
-def read_conf_file(confile):
-    global file_param
-    if os.path.isfile(confile) is True:
-        with open(confile, 'r') as f:
-            data_file = f.readlines()
-            for line in data_file:
-                # skip empty line in conf file or commented line
-                if line.strip() != '':
-                    file_param[line.split('=')[0].strip()] = line.split('=')[1].split('\n')[0].strip()
-    else:
-        print('file', confile, ' not found')
-        quit()
+class MQTTRemoteSchedule:
+    def __init__(self, master_topic, pub_topics, msg_topic, device_name, broker='192.168.2.113', qos=0, active=True):
+        self.param_file, self.confile_loc = None, None
+        self.def_sched_down_1, self.def_sched_down_2 = {}, {}
+        self.def_sched_up_1, self.def_sched_up_2 = {}, {}
+        self.pub_topics, self.msg_topic = [pub_topics, master_topic + '_SCHD'], msg_topic
+        self.broker, self.master_topic = broker, master_topic
+        self.active_schedule_flag = active
+        self.boot_time = datetime.datetime.now()
 
-def confile_path_cmdline():
-    global confloc
-    argv = sys.argv[1:]
-    if len(argv) > 0:
-        confloc = argv[0]
+        self.start_mqtt_service(device_name, qos)
+        self.default_schedules()
+        self.start_up_schedule()
+        self.start_down_schedule()
 
+    def start_mqtt_service(self, device_name, qos):
+        self.mqtt_agent = MQTTClient(sid=device_name, topics=self.pub_topics, topic_qos=qos, host=self.broker)
+        self.mqtt_agent.call_externalf = lambda: self.mqtt_commands(self.mqtt_agent.arrived_msg)
+        self.mqtt_agent.start()
+        sleep(1)
 
+        self.pub_msg(msg_topic=self.msg_topic, msg='Schedule is UP')
 
-def start_mqtt_service():
-    global mqtt_agent, MQTT_HOST, DEVICE_NAME, DEVICE_TOPIC, MSG_TOPIC, SCH_TOPIC
-    
-    topics = [SCH_TOPIC, GROUP_TOPIC]
-    mqtt_agent = MQTTClient(sid=DEVICE_NAME, topics=topics, topic_qos=0, host=MQTT_HOST)
-    mqtt_agent.call_externalf = lambda: mqtt_commands(mqtt_agent.arrived_msg)
-    mqtt_agent.start()
-    sleep(1)
-    pub_msg(msg_topic=MSG_TOPIC,msg='Schedule is UP')
+    def mqtt_commands(self, msg):
+        msg_codes = [0, 1, 2, 3, 4, 5, 6]
+        msg_text = ['UP', 'DOWN', 'OFF', 'STATUS', 'DIS_SHCD', 'ENB_SCHD', 'REPORT']
+        if msg.upper() == msg_text[0] or msg_codes[0]:
+            self.pub_msg(msg_text[0].lower())
+        elif msg.upper() == msg_text[1] or msg.upper() == msg_codes[1]:
+            self.pub_msg(msg_text[1].lower())
+        elif msg.upper() == msg_text[2] or msg.upper() == msg_codes[2]:
+            self.pub_msg(msg_text[2].lower())
+        elif msg.upper() == msg_text[3] or msg.upper() == msg_codes[3]:
+            self.pub_msg(msg_text[3].lower())
+        elif msg.upper() == msg_text[4] or msg.upper() == msg_codes[4]:
+            self.pub_msg(msg_text[4].lower())
+        elif msg.upper() == msg_text[5] or msg.upper() == msg_codes[5]:
+            self.pub_msg(msg_text[5].lower())
+        elif msg.upper() == msg_text[6] or msg.upper() == msg_codes[6]:
+            a = self.schedule_up.tasks_descriptive()
+            print(a)
+            # self.pub_msg(msg_text[6].lower())
+        else:
+            pass
 
-def mqtt_commands(msg):
-    if msg.upper() == 'UP':
-        pub_msg('up')
-    elif msg.upper() == 'OFF':
-        pub_msg('off')
-    elif msg.upper() == 'DOWN':
-        pub_msg('down')
-    elif msg.upper() == 'STATUS':
-        pub_msg(state_0 + '; ' + state_1)
-    else:
-        pass
+    def pub_msg(self, msg, msg_topic=None):
+        if msg_topic == None:
+            msg_topic = self.master_topic
+        else:
+            time_stamp = '[' + str(datetime.datetime.now())[:-5] + ']'
+            msg = '%s [%s] %s' % (time_stamp, self.master_topic.split('/')[-1], msg)
 
-def pub_msg(msg,msg_topic=None):
-    global MSG_TOPIC, DEVICE_TOPIC
-    if msg_topic == None:
-        msg_topic = DEVICE_TOPIC
-    else:
-        msg_topic = MSG_TOPIC
-    mqtt_agent.pub(payload=msg, topic=msg_topic)
+        self.mqtt_agent.pub(payload=msg, topic=msg_topic)
 
-def conv_list_int(list1):
-    a=list1.split(',')
-    aa=[int(i) for i in a]
-    return aa
-    
-def up_schedule():
-    global UP_START_DAYS,UP_END_DAYS,UP_START_TIME, UP_END_TIME
-    schedule_up = scheduler.RunWeeklySchedule(on_func=sched_on_func, off_func=sched_off_func)
-    schedule_up.add_weekly_task(new_task={'start_days': UP_START_DAYS, 'start_time': UP_START_TIME,
-     'end_days': UP_END_DAYS, 'end_time': UP_END_TIME})
-    schedule_up.start()
-    
-def down_schedule():
-    global DOWN_START_DAYS,DOWN_END_DAYS,DOWN_START_TIME, DOWN_END_TIME
-    schedule_down = scheduler.RunWeeklySchedule(on_func=sched_down_func, off_func=sched_off_func)
-    schedule_down.add_weekly_task(new_task={'start_days': DOWN_START_DAYS, 'start_time': DOWN_START_TIME,
-    'end_days': DOWN_END_DAYS, 'end_time': DOWN_END_TIME})
-    schedule_down.start()
+    def start_up_schedule(self):
+        self.schedule_up = scheduler.RunWeeklySchedule(on_func=lambda: self.pub_msg('up'),
+                                                       off_func=lambda: self.pub_msg('off'))
+        self.schedule_up.add_weekly_task(new_task=self.def_sched_up_1)
+        self.schedule_up.add_weekly_task(new_task=self.def_sched_up_2)
+        self.schedule_up.start()
 
-def sched_on_func():
-    #pub_msg("up")
-    print("up")
-    
-def sched_off_func():
-    #pub_msg("off")
-    print("off")
-    
-def sched_down_func(): 
-    #pub_msg("down")
-    print("down")
- 
- 
-################## Config file Parameters ########## 
-file_param={}       
-confloc = '/home/guy/github/LocalSwitch/rooms/FamilyRoomWindow/'                  
-confile_path_cmdline()
-confile_name = 'HomePi_SCH.conf'
-read_conf_file(confloc + confile_name)                                                                                              
-DEVICE_NAME = file_param['DEVICE_NAME']
-MQTT_HOST = '192.168.2.113'
-# To send msg to All
-MSG_TOPIC = file_param['MSG_TOPIC'] 
-# To communicate with this MQTT regrading SCH
-SCH_TOPIC = file_param['SCH_TOPIC'] 
-# Send SCH operation to HW 
-DEVICE_TOPIC = file_param['DEVICE_TOPIC'] 
-GROUP_TOPIC = file_param['GROUP_TOPIC'] 
-UP_START_DAYS = conv_list_int(file_param['up_start_days'])
-UP_START_TIME = file_param['up_start_time']
-UP_END_DAYS = conv_list_int(file_param['up_end_days'])
-UP_END_TIME = file_param['up_end_time']
-DOWN_START_DAYS = conv_list_int(file_param['down_start_days'])
-DOWN_START_TIME = file_param['down_start_time']
-DOWN_END_DAYS = conv_list_int(file_param['down_end_days'])
-DOWN_END_TIME = file_param['down_end_time']
+    def start_down_schedule(self):
+        self.schedule_down = scheduler.RunWeeklySchedule(on_func=lambda: self.pub_msg('down'),
+                                                         off_func=lambda: self.pub_msg('off'))
+        self.schedule_down.add_weekly_task(new_task=self.def_sched_down_1)
+        self.schedule_down.add_weekly_task(new_task=self.def_sched_down_2)
+        self.schedule_down.start()
 
-####################################################
+    def default_schedules(self):
+        self.def_sched_up_1 = {'start_days': [1, 2, 3, 4, 5], 'start_time': '06:30:00',
+                               'end_days': [1, 2, 3, 4, 5], 'end_time': '21:30:05'}
+        self.def_sched_up_2 = {'start_days': [1, 2, 3, 4, 5, 6, 7], 'start_time': '02:00:00',
+                               'end_days': [1, 2, 3, 4, 5, 6, 7], 'end_time': '02:00:05'}
 
-####### Start Here ########
-start_mqtt_service()
-up_schedule()
-down_schedule()
+        self.def_sched_down_1 = {'start_days': [1, 2, 3, 4, 5], 'start_time': '08:00:00',
+                                 'end_days': [1, 2, 3, 4, 5], 'end_time': '08:00:59'}
+        self.def_sched_down_2 = {'start_days': [1, 2, 3, 4, 5, 6, 7], 'start_time': '01:59:00',
+                                 'end_days': [1, 2, 3, 4, 5, 6, 7], 'end_time': '01:59:59'}
 
 
-
+first_sucker = MQTTRemoteSchedule(master_topic='HomePi/Dvir/Windows/ESP32', pub_topics='HomePi/Dvir/Windows/SCHDS',
+                                  device_name='SCH', msg_topic='HomePi/Dvir/Messages')
