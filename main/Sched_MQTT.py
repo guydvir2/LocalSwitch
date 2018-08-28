fea0be5b756a593913293caf9174ce7d484acfe0
guyd@rpi3 <guydvir.tech.com>
2018-08-28 10:30:13 +0300

    after fixing MQTT_SCHED

--- main/Sched_MQTT.py
+++ main/Sched_MQTT.py
@@ -12,6 +12,10 @@ path.append(main_path)
 import scheduler
 from mqtt_switch import MQTTClient
 
+import paho.mqtt.client as mqtt
+from threading import Thread
+
+
 
 class MQTTRemoteSchedule:
     def __init__(self, master_topic, pub_topics, msg_topic, device_name=None, broker='192.168.2.113', qos=0,
@@ -29,6 +33,8 @@ class MQTTRemoteSchedule:
         self.default_schedules()
         self.start_up_schedule()
         self.start_down_schedule()
+        #sleep(1)
+        #self.PBit()
 
     def start_mqtt_service(self, device_name, qos):
         self.mqtt_agent = MQTTClient(sid=device_name, topics=self.pub_topics, topic_qos=qos, host=self.broker)
@@ -93,17 +99,83 @@ class MQTTRemoteSchedule:
         self.schedule_down.start()
 
     def default_schedules(self):
-        self.def_sched_up_1 = {'start_days': [1, 2, 3, 4, 5], 'start_time': '06:05:00',
-                               'end_days': [1, 2, 3, 4, 5], 'end_time': '06:06:05'}
+        self.def_sched_up_1 = {'start_days': [1, 2, 3, 4, 5], 'start_time': '10:06:00',
+                               'end_days': [1, 2, 3, 4, 5], 'end_time': '10:06:05'}
         self.def_sched_up_2 = {'start_days': [1, 2, 3, 4, 5, 6, 7], 'start_time': '02:00:00',
                                'end_days': [1, 2, 3, 4, 5, 6, 7], 'end_time': '02:00:05'}
 
-        self.def_sched_down_1 = {'start_days': [1, 2, 3, 4, 5], 'start_time': '08:00:00',
-                                 'end_days': [1, 2, 3, 4, 5], 'end_time': '08:00:59'}
+        self.def_sched_down_1 = {'start_days': [1, 2, 3, 4, 5], 'start_time': '10:06:30',
+                                 'end_days': [1, 2, 3, 4, 5], 'end_time': '10:06:59'}
         self.def_sched_down_2 = {'start_days': [1, 2, 3, 4, 5, 6, 7], 'start_time': '01:59:00',
                                  'end_days': [1, 2, 3, 4, 5, 6, 7], 'end_time': '01:59:59'}
 
+    def PBit(self):
+        self.pub_msg('up')
+        sleep(0.5)
+        self.pub_msg('down')
+        sleep(0.5)
 
-Home_Devices = ['HomePi/Dvir/Windows/ESP32', 'HomePi/Dvir/Windows/S2-RoomWindow', 'HomePi/Dvir/Windows/S1-RoomWindow', 'HomePi/Dvir/Windows/P-RoomWindow']
+        
+class MQTTlogger:
+    def __init__(self, sid=None, host="192.168.2.113", username=None, 
+                        password=None, topics=None, topic_qos=None, filename='/home/guy/MQTT.log'):
+        Thread.__init__(self)
+        self.sid = sid
+        self.host = host
+        self.filename = filename
+        self.username = username
+        self.password = password
+        self.topics = topics
+        self.topic_qos = topic_qos
+        self.client, self.arrived_msg = None, None
+        
+        self.header
+        
+    def header(self):
+        topics_text = [topic for i, topic in enumerate (self.topics) ]
+        line1="MQTT logger - Topics:"
+            
+
+    def on_connect(self, client, obj, flags, rc):
+        print(">> Connecting to MQTT server %s: %d" % (self.host, rc))
+        for topic in self.topics:
+            print(">> Subscribe topic: %s" % topic)
+            self.client.subscribe(topic, qos=self.topic_qos)
+
+    def on_message(self, client, obj, msg):
+        self.arrived_msg = msg.payload.decode()
+        
+    def timeStamp(self):
+        return str(datetime.datetime.now())[:-5]
+        
+    def check_logfile_valid(self):
+        if os.path.isfile(self.filename) is True:
+            self.valid_logfile = True
+        else:
+            open(self.filename, 'a').close()
+            self.valid_logfile = os.path.isfile(self.filename)
+            if self.valid_logfile is True:
+                msg = '>>Log file %s was created successfully' % self.filename
+            else:
+                msg = '>>Log file %s failed to create' % self.filename
+            print(msg)
+            self.append_log(msg, time_stamp=1)
+
+    def append_log(self, log_entry=''):
+        self.msg = '[%s] %s' % (self.time_stamp(), log_entry)
+
+        if self.valid_logfile is True:
+            myfile = open(self.filename, 'a')
+            myfile.write(self.msg + '\n')
+            myfile.close()
+        else:
+            print('Log err')
+        if self.output2screen == 1:
+            print(self.msg)
+        
+        
+
+Home_Devices = ['HomePi/Dvir/Windows/ESP32_NEW', 'HomePi/Dvir/Windows/ESP32_3']
 for client in Home_Devices:
     MQTTRemoteSchedule(master_topic=client, pub_topics='HomePi/Dvir/Windows/SCHDS', msg_topic='HomePi/Dvir/Messages')
+#a=MQTTlogger(sid="test",topics = ['HomePi/Dvir/Messages','HomePi/Dvir/Windows/All'])
