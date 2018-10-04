932d99c909b15f798a6fbb1d0c47cbbd969c08c5
Guy Dvir <guydvir2@gmail.com>
2018-10-04 20:31:53 +0300

    updates

--- boiler.py
+++ boiler.py
@@ -6,17 +6,18 @@ class MainGUI(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self)
         self.master = master
+        self.master.title("HotWater Boiler Controller")
         self.label1 = tk.StringVar()
         self.label2 = tk.StringVar()
-        self.label1.set("First line")
-        self.label2.set("OFF")
+
         self.op_state = tk.BooleanVar()
         self.op_state.set(False)
         self.op_timer = tk.IntVar()
         self.op_timer.set(0)
+
         self.frame = tk.Frame(self.master)
         self.frame.grid()
-        self.start_stop_button = tk.Button(self.frame, text="Start", command=self.on_off_cb)
+        self.start_stop_button = tk.Button(self.frame, text="Start", command=self.on_off_cb, width=15)
         self.start_stop_button.grid(row=2, column=0)
         self.timer_button = tk.Button(self.frame, text="Timer", command=self.time_cb)
         self.timer_button.grid(row=2, column=1)
@@ -29,6 +30,7 @@ class MainGUI(tk.Frame):
         self._clock_loop = None
         self._timer_loop = None
         self._timer2_loop = None
+        self.timeRemain_counter = None
 
         self.off_state()
 
@@ -41,11 +43,18 @@ class MainGUI(tk.Frame):
             self.op_state.set(False)
 
     def time_cb(self):
-        if self.op_timer.get() == 0:
-            self.timer_start = datetime.datetime.now()
-            self.op_timer.set(self.op_timer.get() + 1)
-            self.label2.set("Timer")
-            self.timer_counter()
+        if self.op_state.get() is True:
+            if self.op_timer.get() == 0:
+                self.timer_start = datetime.datetime.now()
+                self.label1.set("On + Timer")
+                self.op_timer.set(self.op_timer.get() + 1)
+                self.stop_clock()
+                self.stop_counter()
+                self.timer_counter()
+            elif 12 >= self.op_timer.get() > 0:
+                self.op_timer.set(self.op_timer.get() + 1)
+            elif self.op_timer.get() > 13:
+                self.op_timer.set(0)
 
     def clock_update(self):
         time = str(datetime.datetime.now())[:-7]
@@ -58,14 +67,29 @@ class MainGUI(tk.Frame):
     def stop_counter(self):
         root.after_cancel(self._timer_loop)
 
+    def stop_timer(self):
+        root.after_cancel(self._timer2_loop)
+        print("STOP TIMER")
+
     def counter(self):
         time_delta = datetime.datetime.now() - self.on_start
         self.label2.set(str(time_delta)[:-5])
         self._timer_loop = root.after(500, self.counter)
 
     def timer_counter(self):
-        remain_time = self.timer_start + datetime.timedelta(minutes=10 * self.op_timer.get()) - datetime.datetime.now()
-        self.label2.set(str(remain_time)[:-3])
+        def run():
+            self.timeRemain_counter = self.timer_start + datetime.timedelta(
+                seconds=10 * self.op_timer.get()) - datetime.datetime.now()
+            self.label2.set(str(self.timeRemain_counter)[:-5])
+
+        if self.op_timer.get() > 0 and self.timeRemain_counter.total_seconds() > 0:
+            print("RUN")
+            run()
+        elif self.timeRemain_counter.total_seconds() <= 0:
+            self.timeRemain_counter = None
+            self.stop_timer()
+            self.turn_off_device()
+            self.op_timer.set(0)
         self._timer2_loop = root.after(500, self.timer_counter)
 
     def off_state(self):
@@ -79,6 +103,10 @@ class MainGUI(tk.Frame):
         self.label1.set("On")
         self.stop_clock()
         self.counter()
+
+    def turn_off_device(self):
+        root.after_cancel(self._timer2_loop)
+        self.off_state()
 
 
 root = tk.Tk()
